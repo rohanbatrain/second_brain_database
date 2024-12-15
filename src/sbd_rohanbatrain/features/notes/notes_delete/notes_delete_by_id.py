40c297548c8c675baa94830c54dfb79a898a4462
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-12-15 13:04:18 +0530

    making changes to simplify the database operations

--- src/sbd_rohanbatrain/features/notes/notes_delete/notes_delete_by_id.py
+++
@@ -1,8 +0,0 @@
-def delete_note(note_id):
-    # Delete the note from MongoDB
-    result = notes_collection.delete_one({"_id": note_id})
-    
-    if result.deleted_count > 0:
-        print(f"Note {note_id} deleted.")
-    else:
-        print("Note not found.")
