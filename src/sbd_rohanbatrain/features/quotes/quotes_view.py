3234db5a94877c27e6c815d5a44041c07fd5f34a
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-12-26 18:33:56 +0530

    Restarted after a few experiments

--- src/sbd_rohanbatrain/features/quotes/quotes_view.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-from sbd_rohanbatrain.database.db import quotes_collection
-
-def get_quote_by_id(quote_id):
-    """Retrieve a quote by its ID."""
-    return quotes_collection.find_one({"_id": quote_id})
