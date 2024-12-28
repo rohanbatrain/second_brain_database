8f9cffbd8de34f975483093c029b64d00f9b67f7
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-12-28 15:37:05 +0530

    Add authorization token and update plan names in bash test scripts

--- tests/using-bash/update_plan_project_limit.sh
+++ tests/using-bash/update_plan_project_limit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+Token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlckBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoidGVzdHVzZXIiLCJlbWFpbCI6InRlc3R1c2VyQGV4YW1wbGUuY29tIiwiZXhwIjoxNzQ4MzM5MDgwfQ.iV8Txdw-W3-NnxtlU61bj3ehK68fh-aBLArOhKtPRwA"
 curl -X POST http://localhost:5000/admin/plan/update_plan \
 -H "Content-Type: application/json" \
+-H "Authorization: Bearer $Token" \
 -d '{
-    "name": "Plan Name",
+    "name": "Premium",
     "project_limit": 10
 }'
+
